dbc1cb79c5f1417005715a6391ea796e8b7684db
Lucía Seoane <luciaseo20@gmail.com>
2026-03-04 18:31:17 +0100

    update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import logo from "/images/guitarLogo.png";
 
 export default function Header() {
   const { i18n } = useTranslation();
@@ -13,7 +14,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 w-full z-50 bg-neutral-900/80 backdrop-blur border-b border-neutral-800">
       <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-12 h-16 flex items-center justify-between">
-        <h1 className="text-lg font-semibold tracking-wide">Logo</h1>
+        <img src={logo} alt="Brand Logo" className="h-20 w-auto" />
 
         <div className="flex gap-4 text-xs tracking-widest uppercase">
           <button
